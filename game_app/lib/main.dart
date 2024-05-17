5d1798dda736f4d0ebd8150ce7eea0995711f3fa
aymen20014ali <manal20014@gmail.com>
2024-05-17 20:33:44 +0300

    my code

--- game_app/lib/main.dart
+++ game_app/lib/main.dart
@@ -1,125 +1,105 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flame/camera.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/foundation.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:quizzler/dino/game/dino_run.dart';
+import 'package:quizzler/dino/models/player_data.dart';
+import 'package:quizzler/dino/models/settings.dart';
+import 'package:quizzler/dino/widgets/game_over_menu.dart';
+import 'package:quizzler/dino/widgets/hud.dart';
+import 'package:quizzler/dino/widgets/main_menu.dart';
+import 'package:quizzler/dino/widgets/pause_menu.dart';
+import 'package:quizzler/dino/widgets/settings_menu.dart';
+import 'package:quizzler/firebase_options.dart';
+import 'package:quizzler/screens/chat_screen.dart';
+import 'package:quizzler/screens/main_chat_screen.dart';
+import 'package:quizzler/screens/registration_screen.dart';
+import 'package:quizzler/screens/signin_screens.dart';
+import 'package:quizzler/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hive/hive.dart';
 
-void main() {
-  runApp(const MyApp());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await initHive();
+  runApp(MyApp());
+}
+
+Future<void> initHive() async {
+  // For web hive does not need to be initialized.
+  if (!kIsWeb) {
+    final dir = await getApplicationDocumentsDirectory();
+    Hive.init(dir.path);
+  }
+
+  Hive.registerAdapter<PlayerData>(PlayerDataAdapter());
+  Hive.registerAdapter<Settings>(SettingsAdapter());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final _auth = FirebaseAuth.instance;
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Dino Run',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        fontFamily: 'Audiowide',
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        // Settings up some default theme for elevated buttons.
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            fixedSize: const Size(200, 60),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: Scaffold(
+        body: GameWidget<DinoRun>.controlled(
+          // This will dislpay a loading bar until [DinoRun] completes
+          // its onLoad method.
+          loadingBuilder: (conetxt) => const Center(
+            child: SizedBox(
+              width: 200,
+              child: LinearProgressIndicator(),
+            ),
+          ),
+          // Register all the overlays that will be used by this game.
+          overlayBuilderMap: {
+            MainMenu.id: (_, game) => MainMenu(game),
+            PauseMenu.id: (_, game) => PauseMenu(game),
+            Hud.id: (_, game) => Hud(game),
+            GameOverMenu.id: (_, game) => GameOverMenu(game),
+            SettingsMenu.id: (_, game) => SettingsMenu(game),
+          },
+          // By default MainMenu overlay will be active.
+          initialActiveOverlays: const [MainMenu.id],
+          gameFactory: () => DinoRun(
+            // Use a fixed resolution camera to avoid manually
+            // scaling and handling different screen sizes.
+            camera: CameraComponent.withFixedResolution(
+              width: 360,
+              height: 180,
+            ),
+          ),
+        ),
+      ),
+      // initialRoute: _auth.currentUser != null ? MainChatScreen.screenRoute : WelcomeScreen.screenRoute,
+      routes: {
+        WelcomeScreen.screenRoute: (context) => WelcomeScreen(),
+        Registrationscreen.screenRoute: (context) => Registrationscreen(),
+        chatScreen.screenRoute: (context) => chatScreen(),
+        signinscreens.screenRoute: (context) => signinscreens(),
+        MainChatScreen.screenRoute: (context) => MainChatScreen(),
+      },
     );
   }
 }
